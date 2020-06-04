c575d9bb3dbe5e9d4c6fe0c9974c342e21915eac
wangweiguang@stevegame.cn <wangweiguang@stevegame.cn>
2020-06-04 11:42:09 +0800

    异步维表Join

--- src/main/java/com/sdf/flink/streaming/FlinkStreamingDimAsyncJoin.java
+++ src/main/java/com/sdf/flink/streaming/FlinkStreamingDimAsyncJoin.java
@@ -20,7 +20,6 @@ import org.slf4j.LoggerFactory;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
